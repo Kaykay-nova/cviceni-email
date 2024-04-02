5768130993fc56e979cf199946f122ac6597b674
Kristýna Konečná <kristyna.konecna02@gmail.com>
2024-04-02 19:06:44 +0200

    Přidána funkce na pozdrav

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 
+const goodbye = (jmenoUzivatele) => {
+  const pozdrav = document.body.querySelector('.email__closing')
+  pozdrav.innerHTML = 'S pozdravem, ' + jmenoUzivatele
+};
+
+goodbye('Pavel Veselý');
