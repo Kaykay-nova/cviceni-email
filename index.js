8a474e3cce35445d4e5e659522a87c34e98645ca
Kristýna Konečná <kristyna.konecna02@gmail.com>
2024-04-02 19:31:18 +0200

    Přidání funkcí, úprava body

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-
-const goodbye = (jmenoUzivatele) => {
+const goodbye = (namePerson) => {
   const pozdrav = document.body.querySelector('.email__closing')
-  pozdrav.innerHTML = 'S pozdravem, ' + jmenoUzivatele
+  pozdrav.innerHTML = 'S pozdravem, ' + namePerson
 };
 
+const fillSubject = (subject) => {
+  const emailSubject = document.body.querySelector('.email__subject')
+  emailSubject.innerHTML = subject
+};
+
+const fillBody = (body, namePerson) => {
+  const emailBody = document.body.querySelector('.email__body')
+  emailBody.innerHTML = body
+
+  const name = document.body.querySelector('.email__closing')
+  name.innerHTML = 'S pozdravem, ' + namePerson
+
+}
+
 goodbye('Pavel Veselý');
+fillSubject('Odpověď na nabídku práce');
+fillBody('Chci práci!', 'Eliška Krásnohorská');
